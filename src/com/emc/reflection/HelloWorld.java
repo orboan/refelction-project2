b5c5fa4b54709b4ad51932b11a6352f1c2d93426
Oriol Boix Anfosso <orboan@ilg.cat>
2022-10-20 08:45:00 +0000

    first commit

---
+++ src/com/emc/reflection/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.emc.reflection;
+
+public class HelloWorld {
+	
+	public void sayHello() {
+		System.out.printf("Hello World %n");
+	}
+	
+	public void sayHello(String name) {
+		System.out.printf("Hello %s%n", name);
+	}
+
+}
